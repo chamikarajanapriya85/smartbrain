4c6097ca1f3899ad4cb03ea26a4efd5f4fd82f92
chamikarajanapriya <chamikarajanapriya@yahoo.com>
2021-10-07 19:08:10 +0530

    add Navigation component

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav style={{ display: "flex", justifyContent: "flex-end" }} />
+      <p>Sign In</p>
+      <nav />
+    </div>
+  );
+};
+
+export default Navigation;
